112d2bf2613b4d57ebe8be56bd32de023169e00c
VutinanKannim <wuttinun.g@ku.th>
2020-04-16 20:59:54 +0700

    Bills2

--- Bills2/Bills2/Form1.Designer.cs
+++ Bills2/Bills2/Form1.Designer.cs
@@ -36,15 +36,17 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(361, 182);
+            this.textBox1.Location = new System.Drawing.Point(347, 182);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(152, 178);
+            this.textBox1.Size = new System.Drawing.Size(155, 167);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
@@ -53,53 +55,75 @@
             this.textBox2.Location = new System.Drawing.Point(30, 182);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(174, 178);
+            this.textBox2.Size = new System.Drawing.Size(161, 167);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(221, 182);
+            this.textBox3.Location = new System.Drawing.Point(214, 182);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 178);
+            this.textBox3.Size = new System.Drawing.Size(112, 167);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(533, 182);
+            this.textBox4.Location = new System.Drawing.Point(521, 182);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 178);
+            this.textBox4.Size = new System.Drawing.Size(96, 167);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(649, 182);
+            this.textBox5.Location = new System.Drawing.Point(634, 182);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(110, 178);
+            this.textBox5.Size = new System.Drawing.Size(133, 167);
             this.textBox5.TabIndex = 4;
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(777, 182);
+            this.textBox6.Location = new System.Drawing.Point(778, 182);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 178);
+            this.textBox6.Size = new System.Drawing.Size(100, 167);
             this.textBox6.TabIndex = 5;
             this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(118, 417);
+            this.textBox7.Location = new System.Drawing.Point(113, 398);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(102, 33);
+            this.textBox7.Size = new System.Drawing.Size(145, 33);
             this.textBox7.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("RaiNgan", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(124, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("RaiNgan", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(353, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 43);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "รายะเอียดสินค้า";
             // 
             // Form1
             // 
@@ -107,6 +131,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(900, 550);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -132,6 +158,8 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
