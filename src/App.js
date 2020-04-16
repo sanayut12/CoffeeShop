204f2a86ecb62a972530a710d9ffa5a3449ff3bb
sanayut12 <sanayut12@gmail.com>
2020-04-16 17:09:29 +0700

    web page home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Jumbotron, Container,Badge ,Button,ButtonGroup} from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Jumbotron className="Jumbotron">
+            <h1><Badge>Coffee Home </Badge></h1>
+            <p> <center><h3 className="h3">เว็บสำหรับสั่งอาหารของทางร้าน</h3></center>  </p>
+        </Jumbotron>
+      </div>
+
+      <div>
+          <ButtonGroup>
+                <Button>หน้าหลัก</Button>
+                <Button>เมนู</Button>
+                <Button>promotions</Button>
+                <Button>profile</Button>
+                <Button>register</Button>
+                <Button>login</Button>
+          </ButtonGroup>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//  <header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
+// </header>
